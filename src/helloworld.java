5675019c13ba1d4c2b3732bf4d355d09132989a3
Joshua Vang <vangjosh95@gmail.com>
2022-08-30 21:07:54 -0500

    made some corrections to the java file

--- src/helloworld.java
+++ src/helloworld.java
@@ -2,7 +2,7 @@ public class helloworld{
 
 public static void main(String[] args){
   System.out.println("I edit some brand new stuff to GITHUB");
-  System.out.println("IF you can see THIS! This means it work perfectly!"):
+  System.out.println("IF you can see THIS! This means it work perfectly!");
 
 }
 
