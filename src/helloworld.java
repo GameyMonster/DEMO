86c4d0bdda87304c81ba1073298cc6c8371012a5
GameyMonster <44377094+GameyMonster@users.noreply.github.com>
2022-08-30 21:06:47 -0500

    Create helloworld.java
    create a hello class

---
+++ src/helloworld.java
@@ -0,0 +1,10 @@
+public helloworld{
+
+public static void main(String[] args){
+  System.out.println("I edit some brand new stuff to GITHUB");
+  System.out.println("IF you can see THIS! This means it work perfectly!"):
+
+}
+
+
+}
